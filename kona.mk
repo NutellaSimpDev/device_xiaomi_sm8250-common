578fdde73d334df0e4a4a39ff14abae4c84cd430
NutellaDev <eduardocayovilca@gmail.com>
2023-08-19 03:32:00 -0500

    sm8250-common: audio: Decommonize some audio configs

--- kona.mk
+++ kona.mk
@@ -139,9 +139,6 @@ PRODUCT_PACKAGES += \
 
 # Audio configs
 PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
-
-PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
 
 PRODUCT_COPY_FILES += \
